a2b3b32f56f5effdb613211b4dbf0a268547f9a7
Divx1087 <65459715+Divx1087@users.noreply.github.com>
2023-05-03 14:12:32 +0200

    made registration page
    registration page made in portrait mode, nothing fancy, but works fine.
    will be better soonish.. :eyes:

--- app/src/main/java/com/example/museum/MainActivity.java
+++ app/src/main/java/com/example/museum/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.museum;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,9 +12,13 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getName();
+    private static final String PREF_KEY = MainActivity.class.getPackage().toString();
+    private static final int SECRET_KEY = 99;
 
     EditText userNameET;
     EditText passwordET;
+
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
 
         userNameET = findViewById(R.id.editTextUserName);
         passwordET = findViewById(R.id.editTextPassword);
+
+        preferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE);
+
+        Log.i(LOG_TAG, "onCreate");
     }
 
     public void login(View view) {
@@ -30,5 +40,46 @@ public class MainActivity extends AppCompatActivity {
         String password = passwordET.getText().toString();
 
         Log.i(LOG_TAG, "bejelentkezett: " + userName + ", jelszó: " + password);
+    }
+
+    public void registration(View view) {
+        Intent intent = new Intent(this, RegistrationActivity.class);
+        intent.putExtra("SECRET_KEY", SECRET_KEY);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("userName", userNameET.getText().toString());
+        editor.putString("password", passwordET.getText().toString());
+        editor.apply();
+
+        Log.i(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(LOG_TAG, "onResume");
     }
 }
---
+++ app/src/main/java/com/example/museum/RegistrationActivity.java
@@ -0,0 +1,106 @@
+package com.example.museum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = RegistrationActivity.class.getName();
+    private static final String PREF_KEY = MainActivity.class.getPackage().toString();
+
+    EditText userNameEditText;
+    EditText userEmailEditText;
+    EditText userPasswordEditText;
+    EditText userPasswordAgainEditText;
+    EditText userPhoneEditText;
+
+    private SharedPreferences preferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        // Bundle bundle = getIntent().getExtras();
+        // int secret_key = bundle.getInt("SECRET_KEY");
+        int secret_key = getIntent().getIntExtra("SECRET_KEY", 0);
+
+        if (secret_key != 99 ) {
+            finish();
+        }
+
+        userNameEditText = findViewById(R.id.userNameEditText);
+        userEmailEditText = findViewById(R.id.userEmailEditText);
+        userPasswordEditText = findViewById(R.id.userPasswordEditText);
+        userPasswordAgainEditText = findViewById(R.id.userPasswordAgainEditText);
+        userPhoneEditText = findViewById(R.id.userPhoneEditText);
+
+        preferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE);
+        String userName = preferences.getString("userName", "");
+        String password = preferences.getString("password", "");
+
+        userNameEditText.setText(userName);
+        userPasswordEditText.setText(password);
+        userPasswordAgainEditText.setText(password);
+
+        Log.i(LOG_TAG, "onCreate");
+    }
+
+    public void registration(View view) {
+
+        String userName = userNameEditText.getText().toString();
+        String email = userEmailEditText.getText().toString();
+        String password = userPasswordEditText.getText().toString();
+        String passwordAgain = userPasswordAgainEditText.getText().toString();
+        String phone = userPhoneEditText.getText().toString();
+
+        if (!password.equals(passwordAgain)) {
+            Log.e(LOG_TAG, "Nem egyeznek a megadott jelszavak!");
+            return;
+        }
+
+        Log.i(LOG_TAG, "bejelentkezett: " + userName + ", email: " + email + ", telefonszám: " + phone);
+        //TODO: A regisztrációs funcionalitást meg kell csinálni.
+
+    }
+
+    public void cancel(View view) {
+        finish();
+        Log.i(LOG_TAG, "cancel");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(LOG_TAG, "onResume");
+    }
+}
